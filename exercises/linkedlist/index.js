13a5a2e7281abd36119d19ce1c23e95bf346ed21
onionsalot <trong.nguyen1008@gmail.com>
2021-08-28 16:08:21 -0400

    Day 6 linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,33 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        const node = new Node(data, this.head);
+        this.head = node
+    }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+
+        while(node) {
+            counter++
+            node = node.next;
+        }
+        return counter;
+    }
+}
 
 module.exports = { Node, LinkedList };
